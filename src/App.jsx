7054b65469c8dc10197adcf8ef38c23b237f8631
Xavier <xavieralves162@gmail.com>
2026-03-12 09:46:57 +0000

    Exc 11.8

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
-})))
+const mapResults = (({ results }) =>
+  results.map(({ url, name }) => ({
+    url,
+    name,
+    id: parseInt(url.match(/\/(\d+)\//)[1])
+  }))
+)
 
 const App = () => {
   const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+  const { data: pokemonList, error, isLoading } = useApi(
+    'https://pokeapi.co/api/v2/pokemon/?limit=50',
+    mapResults
+  )
 
   if (isLoading) {
     return <LoadingSpinner />
@@ -27,17 +32,26 @@ const App = () => {
   let previous = null
 
   if (match && match.params) {
-    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-    next = pokemonList.find(({ id }) => id === pokemonId + 1)
+    const currentIndex = pokemonList.findIndex(
+      ({ name }) => name === match.params.name
+    )
+    previous = pokemonList[currentIndex - 1] || null
+    next = pokemonList[currentIndex + 1] || null
   }
 
   return (
     <Routes>
-      <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
-      <Route exact path="/pokemon/:name" element={
-        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-      } />
+      <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
+      <Route
+        path="/pokemon/:name"
+        element={
+          <PokemonPage
+            pokemonList={pokemonList}
+            previous={previous}
+            next={next}
+          />
+        }
+      />
     </Routes>
   )
 }
